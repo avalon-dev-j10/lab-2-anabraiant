7bee5360981639be3ffe99451a3d66d7926e8331
Ana Braiant <0533359@gmail.com>
2019-03-28 20:12:13 +0300

    Lab03

---
+++ lab-3-anabraiant-master/src/ru/avalon/java/dev/j10/labs/initialization/RandomInitializer.java
@@ -0,0 +1,45 @@
+package ru.avalon.java.dev.j10.labs.initialization;
+
+import java.util.Random;
+import ru.avalon.java.dev.j10.labs.Initializer;
+
+/**
+ * Инициализатор, выполняющий инициализацию массива
+ * значениями последовательности случайных чисел.
+ *
+ * <p>Экземпляры класса можно использовать многократно для
+ * инициализации любого числа массивов.
+ *
+ * <p>Диапазон, в пределах которого будут получены случайные
+ * значения, передаётся в конструктор класса при
+ * инициализации.
+ */
+public class RandomInitializer implements Initializer {
+    
+    private int a;
+    private int b;
+    
+    public RandomInitializer (int a, int b){
+        this.a = a;
+        this.b = b;
+    }
+    
+
+    /**
+     * Выполняет инициализацию массива, значениями
+     * последовательности случайных чисел.
+     *
+     * @param array массив, подлежащий инициализации
+     */
+    
+    // scopes from A to B (but B doesnt include)
+    public void initialize(int[] array) {
+        for (int i = 0; i < array.length; i++){
+            array[i] = (int) (a + Math.random() * (b-a));
+        }
+        
+        /*
+         * TODO(Студент): Реализовать метод initialize класса RandomInitializer
+         */
+    }
+}
---
+++ lab-3-anabraiant-master/src/ru/avalon/java/dev/j10/labs/sort/ShellSort.java
@@ -0,0 +1,53 @@
+package ru.avalon.java.dev.j10.labs.sort;
+
+import ru.avalon.java.dev.j10.labs.Sort;
+
+/**
+ * Сортировка Шелла (англ. Shell sort).
+ *
+ * <p>Алгоритм сортировки, являющийся усовершенствованным
+ * вариантом сортировки вставками. Идея метода Шелла состоит
+ * в сравнении элементов, стоящих не только рядом, но и на
+ * определённом расстоянии друг от друга. Иными словами —
+ * это сортировка вставками с предварительными «грубыми»
+ * проходами. Аналогичный метод усовершенствования
+ * пузырьковой сортировки называется сортировка расчёской.
+ *
+ * @see <a href="https://ru.wikipedia.org/wiki/%D0%A1%D0%BE%D1%80%D1%82%D0%B8%D1%80%D0%BE%D0%B2%D0%BA%D0%B0_%D0%A8%D0%B5%D0%BB%D0%BB%D0%B0">Сортировка Шелла</a>
+ */
+public class ShellSort implements Sort {
+
+    /**
+     * {@inheritDoc}
+     */
+    public void sort(int[] array) {
+        int temp;
+        int h = 0; //  interval 
+        
+        while (h <= array.length/3) // находим нач.значение интервала!! 
+            h = 3*h +1;
+        for (int k = h; k > 0; k = (k-1)/3)         //на каждой итерации велю интрвала уменьшается 
+            for (int i = k; i < array.length; i++){
+                temp = array[i];
+                int j;
+                for ( j = i; j >=k; j = j-k){
+                    if (temp < array[j-k])
+                        array[j] = array [j - k];
+                    else 
+                        break;}
+                
+                array[j] = temp;
+                
+            }
+        
+        
+        
+       
+        
+        
+        
+        /*
+         * TODO(Студент): Реализовать метод sort класса ShellSort
+         */
+    }  
+}
